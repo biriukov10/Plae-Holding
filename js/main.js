8a631f632c48cec3bd19082a7ea2ac76791216d4
Bogdan B <bogdan@ronis.mail>
2020-02-12 18:57:07 +0200

    add other sections, add slider and start working for pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
 $(document).ready(function(){
-    $(".ph-hero-dots__circle").click(function () {
+  // добавление класса active при прокрутке
+	$(window).scroll(function(){
+		let $sections = $('section');
+   		$sections.each(function(i,el){
+        let top  = $(el).offset().top-100;
+        let bottom = top +$(el).height();
+        let scroll = $(window).scrollTop();
+        let id = $(el).attr('id');
+      if  ( scroll > top && scroll < bottom){
+        $('.active').removeClass('active');
+        $('a[href="#'+id+'"]').addClass('active');
+      }	
+    })
+  });
+
+  // плавный скролл
+  $(".ph-hero-dots__circle").click(function () {
 		let elementClick = $(this).attr("href");
 		let destination = $(elementClick).offset().top;
 		$("body,html").animate({scrollTop: destination }, 800);
 	});
     
+  //slick slider
+
+  $('.ph-range-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: $('.ph-range-left-btn'),
+    nextArrow: $('.ph-range-right-btn'),
+    responsive: [
+      {
+        breakpoint: 970,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      },
+    ]
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  
 });
